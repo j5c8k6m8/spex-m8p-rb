35e681b8c7b7077d3133a068a0e25b332edfc463
j5c8k6m8 <j5c8k6m8@gmail.com>
2023-05-14 22:37:15 +0000

    exec bundle gem spexm8p

---
+++ lib/spexm8p.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "spexm8p/version"
+
+module Spexm8p
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spexm8p/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Spexm8p
+  VERSION = "0.1.0"
+end
